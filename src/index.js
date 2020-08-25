6cf43eaeb07c9eac1b9d710b57b48626f929d6de
Ritesh <ricksrv.5@gmail.com>
2020-08-26 03:01:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ app.use(bodyParser.json())
 
 // Happy birthday kapatia
 
+
+app.get('/', (req, res)=>{
+    res.send("Hello World!");
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app;
